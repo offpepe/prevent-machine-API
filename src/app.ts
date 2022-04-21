eab1d919a6c81531458879db460f36a14ef425e5
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-04-21 20:43:31 -0300

    perf: fix identation

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,23 @@
-import express, { Express,Request, Response } from 'express';
+import express, {Express, Request, Response} from 'express';
 import dotenv from 'dotenv';
 import logger from 'morgan';
 
 dotenv.config()
 
-const { PORT, NODE_ENV } = process.env;
+const {PORT, NODE_ENV} = process.env;
 
-logger.token('localeDate', ():string => {
-    const date:Date = new Date();
-    const minutes:string = date.getUTCMinutes().toString();
-    const normalizedMinutes:string = minutes.length < 2 ? '0' + minutes : minutes;
+logger.token('localeDate', (): string => {
+    const date: Date = new Date();
+    const minutes: string = date.getUTCMinutes().toString();
+    const normalizedMinutes: string = minutes.length < 2 ? '0' + minutes : minutes;
     return `${date.toLocaleDateString('pt-BR')}::${date.getUTCHours() - 3}h${normalizedMinutes}m (brazilian standart time zone)`;
 });
 
-const app:Express = express();
-
+const app: Express = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(logger('[server]: :method --> :status ::: :url (:response-time ms) ::: :localeDate'));
 
-app.get('/',(_req:Request, res:Response) => res.send('Welcome to Prevent Machine API 🤖'));
+app.get('/', (_req: Request, res: Response) => res.send('Welcome to Prevent Machine API 🤖'));
 
 app.listen(PORT, () => console.log(`[server] running on port ${PORT} using ${NODE_ENV} environment`));
