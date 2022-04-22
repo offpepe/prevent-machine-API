86f829c7a7826e4e68a86b68cf7b2ec0106dbf8d
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-04-22 03:25:56 -0300

    fix: updateUser needs userId

--- src/interfaces/services/IUserService.ts
+++ src/interfaces/services/IUserService.ts
@@ -1,12 +1,12 @@
-import { IRegisterUserResponse, IRegisterUserRequest } from "../DTO/IRegisterUser";
+import IRegisterUserRequest from "../DTO/IRegisterUser";
 import IUserDTO from "../DTO/IUserDTO";
 import IDeleteUser from "../DTO/IDeleteUser";
-import {IUpdateUserRequest, IUpdateUserResponse} from "../DTO/IUpdateUser";
+import IUpdateUserRequest from "../DTO/IUpdateUser";
 
 export default interface IUserService {
     Register: (userData: IRegisterUserRequest) => Promise<IUserDTO>;
-    UpdateUser: (userData: IUpdateUserRequest) => Promise<IUserDTO>;
+    UpdateUser: (userData: IUpdateUserRequest, userId: string) => Promise<IUserDTO>;
     GetUser : (userId: string) => Promise<IUserDTO>;
-    ListUsers: () => Promise<[IUserDTO]>;
+    ListUsers: () => Promise<IUserDTO[]>;
     DeleteUser: (userId: string) => Promise<IDeleteUser>;
 }
