d1ab0ebf6f5587362563688face399c6ee97a8fa
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-04-23 03:04:58 -0300

    feat: login endpoint

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -2,10 +2,12 @@ import express, {Request, Router} from "express";
 import UserController from "../controllers/UserController";
 import IUserService from "../models/interfaces/services/IUserService";
 import UserService from "../services/UserService";
+import authMiddleware from "../middlewares/AuthMiddleware";
 
 const controller = new UserController();
 const router: Router = express.Router();
 
+router.post('/login', controller.LoginUser);
 router.post('/', controller.RegisterUser);
 router.get('/', controller.ListUsers);
 router.get('/:id', controller.GetUser);
