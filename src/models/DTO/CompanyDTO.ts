164de25c076de396fde0ea833b0b9c98975283a4
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-04-24 00:10:09 -0300

    feat: company dto's

--- src/models/DTO/CompanyDTO.ts
+++ src/models/DTO/CompanyDTO.ts
@@ -1,4 +1,7 @@
 import IUserDTO from "../interfaces/DTO/IUserDTO";
+import {UserDTO} from "./UserDTO";
+import Company from "../entities/Company";
+import User from "../entities/User";
 
 export default class CompanyDTO {
     companyId: string;
@@ -9,5 +12,18 @@ export default class CompanyDTO {
         this.companyId = companyId;
         this.name = name;
         this.workers = workers;
+    }
+
+    static MapToDTO({id, name, workers}: Company) {
+        const workersDTO: UserDTO[] = workers.map(({id, name, lastName, email, role, company}) =>
+            UserDTO.MapToDTO({
+                id,
+                name,
+                lastName,
+                email,
+                role,
+                company
+            }));
+        return new CompanyDTO(id, name, workersDTO);
     }
 }
---
+++ src/models/DTO/InputNewMemberDTO.ts
@@ -0,0 +1,6 @@
+import Roles from "../enums/Roles";
+
+export default class InputNewMemberDTO {
+    id:string;
+    role: number;
+}
