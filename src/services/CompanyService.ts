c8556036fc558c9868fae0e0d4ab9ecff7ffd192
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-04-24 01:00:21 -0300

    feat: removeMember and removeMemberRange methods

--- src/services/CompanyService.ts
+++ src/services/CompanyService.ts
@@ -3,11 +3,11 @@ import Roles from "../models/enums/Roles";
 import CompanyDTO from "../models/DTO/CompanyDTO";
 import CustomError from "../utils/CustomError";
 import InputNewMemberDTO from "../models/DTO/InputNewMemberDTO";
-
+import RemoveMembersResponse from "../models/DTO/RemoveMembersResponse";
 
 // TODO dependency injection HERE!
 export default class CompanyService {
-    async RegisterCompany(name: string, managerId: string) {
+    public async RegisterCompany(name: string, managerId: string) {
         const user = await prisma.user.findUnique({where: {id: managerId}});
         if (!user) throw CustomError.EntityNotFound('user does not exists');
         await prisma.user.update({
@@ -28,20 +28,12 @@ export default class CompanyService {
         return CompanyDTO.MapToDTO(company);
     }
 
-    async IncludeMembersRange(managerId: string, companyId: string, newMembers: [InputNewMemberDTO]) {
+    public async IncludeMembersRange(managerId: string, companyId: string, newMembers: [InputNewMemberDTO]) {
         try {
-            const manager = await prisma.user.findFirst({
-                where: {
-                    id: managerId,
-                    companyId,
-                    role: Roles.companyManager,
-                }
-            });
-            const company = await prisma.company.findUnique({ where: { id: companyId }, include: { workers: true } });
-            if (!company) throw CustomError.EntityNotFound('company not found');
-            if (!manager) throw CustomError.BadRequest('you need to be manager to include new members on company');
+            await this.ValidateManager(managerId, companyId);
+            await this.CompanyById(companyId);
             await Promise.all(newMembers.map(async (member) => this.IncludeMember(member, companyId)));
-            return CompanyDTO.MapToDTO(company);
+            return CompanyDTO.MapToDTO(await this.CompanyById(companyId));
         } catch (e) {
             console.error(e);
             if (e instanceof CustomError) throw e;
@@ -49,7 +41,7 @@ export default class CompanyService {
         }
     }
 
-    async ListAllMembers() {
+    public async ListAllMembers() {
         try {
             const companies = await prisma.company.findMany({ include: { workers: true } });
             return await Promise.all<CompanyDTO>(companies.map((company) => CompanyDTO.MapToDTO(company)));
@@ -58,7 +50,39 @@ export default class CompanyService {
         }
     }
 
-    private async IncludeMember({ id, role }: InputNewMemberDTO, companyId: string) {
+    public async RemoveMembersRange(managerId: string, companyId: string, users: [string]) {
+        try {
+            await this.ValidateManager(managerId, companyId);
+            await this.CompanyById(companyId);
+            await Promise.all(users.map((user) => this.RemoveMember(companyId, user)));
+            return new RemoveMembersResponse('members removed sucessfully');
+        } catch (e) {
+            if (e instanceof CustomError) throw e;
+            throw CustomError.Internal();
+        }
+    }
+
+
+    protected async RemoveMember(companyId: string, userId: string) {
+        const user = prisma.user.findFirst({
+            where: {
+                id: userId,
+                companyId: companyId,
+            }
+        });
+        if (!user) throw CustomError.BadRequest(`user ${userId} is not part of this company`);
+        await prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: {
+                companyId: null,
+                role: Roles.unregistered,
+            }
+        });
+    }
+
+    protected async IncludeMember({ id, role }: InputNewMemberDTO, companyId: string) {
         const user = await prisma.user.findUnique({
             where: {id},
         });
@@ -78,4 +102,29 @@ export default class CompanyService {
             }
         });
     }
+
+    protected async ValidateManager(managerId: string, companyId: string) {
+        const manager = await prisma.user.findFirst({
+            where: {
+                id: managerId,
+                companyId,
+                role: Roles.companyManager,
+            }
+        });
+        if (!manager) throw CustomError.BadRequest('you need to be a manager to include members on company');
+    }
+
+    protected async CompanyById(companyId: string) {
+        const company = await prisma.company.findUnique({
+            where: {
+                id: companyId,
+            },
+            include: {
+                workers: true,
+            }
+        });
+        if (!company) throw CustomError.EntityNotFound('company not found');
+        return company;
+    }
+
 }
