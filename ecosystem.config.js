55592533266d69575e3aa1431968f3843732cef2
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-04-30 12:54:39 -0300

    config: pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = [{
+    script: 'build/app.js',
+    name: 'prevent_machine_v1',
+    exec_mode: 'cluster',
+    watch: true,
+    merge_logs: true,
+    autorestart: true,
+    instances: 4
+}]
