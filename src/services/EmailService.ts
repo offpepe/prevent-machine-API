554a7af084c486b8a6f47413e0263cfb83d34984
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-05-01 16:03:01 -0300

    config: changed email provider
    from sendGrid to nodeMailer using gmail service

--- src/services/EmailService.ts
+++ src/services/EmailService.ts
@@ -1,32 +1,41 @@
 import dotenv from 'dotenv';
-import sgMail from '@sendgrid/mail';
+import nodemailer from 'nodemailer';
 import {UserDTO} from "../models/DTO/User/UserDTO";
 import AssetDTO from "../models/DTO/Asset/AssetDTO";
+import TransporterSetup from "../models/DTO/Email/TransporterSetup";
 
 dotenv.config();
-const { SENDGRID_API_KEY } = process.env;
+const {GOOGLE_OAUTH2_ACCESS_KEY, SENDER_EMAIL, MAILER_SERVICE} = process.env;
 
 export default class EmailService {
+    private readonly transporter: TransporterSetup;
+    private mailer;
+
     constructor() {
-        sgMail.setApiKey(SENDGRID_API_KEY)
+        this.transporter = new TransporterSetup(MAILER_SERVICE, SENDER_EMAIL, GOOGLE_OAUTH2_ACCESS_KEY);
+        this.mailer = nodemailer.createTransport(this.transporter);
     }
 
     public async SendWarningReport(user: UserDTO, asset: AssetDTO) {
-       const email = {
-           to: user.email,
-           from: 'alan.alb.flopes@gmail.com',
-           subject: `[Prevent-Machine] ATTENTION! Asset ${asset.id} is ${asset.status}`,
-           html: `<h3>Attention, the asset ${asset.id} is ${asset.status}</h3>
+        const email = {
+            to: user.email,
+            from: SENDER_EMAIL,
+            subject: `[Prevent-Machine] ATTENTION! Asset ${asset.id} is ${asset.status}`,
+            html: `
+                <div align="center">
+                <img alt="asset image" src=${asset.image} width="300px" />
+                <h3>Asset status: ${asset.status}</h3>
+                </div>
                 <h4>Asset data:</h4>
                 <ul>
-                <li>- name: ${asset.name};</li>
-                <li>- id: ${asset.id};</li>
-                <li>- model: ${asset.model};</li>
-                <li>- unit: ${asset.owner.id};</li>
-                <li>- unitName: ${asset.owner.name};</li>
+                <li> name: ${asset.name};</li>
+                <li> id: ${asset.id};</li>
+                <li> model: ${asset.model};</li>
+                <li> unit: ${asset.owner.id};</li>
+                <li> unitName: ${asset.owner.name};</li>
                 </ul>
                 <el>With care, Prevent Machine 🩺🤖</el>`,
-       };
-       await sgMail.send(email);
+        };
+        await this.mailer.sendMail(email, (err, info) => err ? console.log(err) : console.log(`[server]: email sent -> ${info.response}`));
     }
 }
